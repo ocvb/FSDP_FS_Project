9f3810024eb56f5b7853bdaf88c622108a6f4e80
seuno <electrokai1@gmail.com>
2024-06-09 00:27:39 +0800

    Footer!

---
+++ front-end/src/components/Footer/Footer.module.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import { Container } from "@mui/material";
+
+import styles from "./css/Footer.module.css";
+
+export default function Footer() {
+    return (
+        <foorter>
+            <div>
+                <p>Footer</p>
+            </div>
+        </foorter>
+    );
+
+}
