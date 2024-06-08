b9eb093782b44f9cb244021da71180305749f71e
seuno <electrokai1@gmail.com>
2024-06-08 19:19:00 +0800

    Added CustomButton

---
+++ front-end/src/components/Button/CustomButton.module.jsx
@@ -0,0 +1,18 @@
+import react from "react";
+
+import { Button } from "@mui/material";
+
+export default function CustomButton(props) {
+
+    return (
+        <Button sx={{
+            display: "inline-flex",
+            borderRadius: "50px",
+            width: "fit-content",
+            boxShadow: "0px 2px 6px 0px rgba(0, 0, 0, 0.15)",
+            padding: "0.5rem 2rem",
+        }} className={props.className} onClick={props.onClick}>
+            {props.text}
+        </Button>
+    );
+}
