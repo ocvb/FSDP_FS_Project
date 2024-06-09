f2ce49cbe4c385d709d774fbdde29cfd3053706f
seuno <electrokai1@gmail.com>
2024-06-09 16:27:27 +0800

    Account Login call api when logged in

--- front-end/src/routes/Account/Account.jsx
+++ front-end/src/routes/Account/Account.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-import { Container, Input } from "@mui/material";
+import { Container, Input, colors, styled } from "@mui/material";
 
 import styles from "./css/Account.module.css";
 
@@ -15,26 +15,51 @@ export default function Account() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                if (!User) {
-                    const response = await axios.get("http://localhost:3001/api/users");
-                    console.log("Data fetched successfully");
-                    // console.log(response.data);
-                    setUser(response.data);
-                }
-            } catch (error) {
-                console.error(error);
-            }
-        };
+    const postData = async () => {
+        try {
+            if (!User) {
+                const response = await axios.post("http://localhost:3001/api/login", {
+                    username: username,
+                    password: password
+                });
 
-        fetchData();
-    }, [User]);
+                console.log("Login data sent");
+                console.log(response.data); // Debugging
+
+                if (response.data['status'] === 'success') {
+                    console.log("Login successful");
+                    setUser(response.data['data']);
+                } else {
+                    console.log("Login failed");
+                }
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
 
     const onLogin = () => {
         setUsername('');
         setPassword('');
+        postData();
+    };
+
+    const InputStyle = {
+        width: "90%",
+        margin: "0.5rem 0",
+        color: "black",
+        backgroundColor: "white",
+        fontSize: "1.2rem",
+        fontFamily: "Lato",
+        '&::after': {
+            borderColor: colors.grey[700],
+        },
+        '& .MuiInputBase-input': {
+            '&::placeholder': {
+                color: colors.grey[800],
+            },
+        },
 
     };
 
@@ -50,26 +75,23 @@ export default function Account() {
                 <div className={styles.container}>
                     <div className={styles.bg}>
                         <img src={bgImage}></img>
-
                     </div>
                     <div className={styles.login}>
                         <div className={styles.loginContainer}>
                             <p>Login</p>
-                            <Input sx={
-                                {
-                                    width: "90%",
-                                    margin: "0.5rem 0",
-                                }
-
-                            } type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)}></Input>
-                            <Input sx={
-                                {
-                                    width: "90%",
-                                    margin: "0.5rem 0",
-                                }
-
-                            } type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}></Input>
-                            <CustomButton text="Login" onClick={onLogin} />
+                            <Input sx={InputStyle} type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)}></Input>
+                            <Input sx={InputStyle} type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}></Input>
+                            <CustomButton text="Login" onClick={onLogin} sx={{
+                                display: "inline-flex",
+                                borderRadius: "10px",
+                                width: "90%",
+                                backgroundColor: "black",
+                                padding: "0.5rem 2rem",
+                                color: "white",
+                                '&:hover': {
+                                    backgroundColor: colors.grey[900],
+                                },
+                            }} />
                         </div>
                     </div>
                 </div>
