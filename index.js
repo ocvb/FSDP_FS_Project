7c940f0b96c99bc68691b7373055a0134a55f702
seuno <electrokai1@gmail.com>
2024-06-11 05:46:23 +0800

    Minor changes to API preset

--- index.js
+++ index.js
@@ -16,7 +16,44 @@ app.use(
 );
 app.use(express.static(path.join(__dirname, "/client/build")));
 
-app.get("/api", (req, res) => {
+app.get("/api", async (req, res) => {
+  const presetUsers = await Users.bulkCreate([
+    {
+      username: "admin",
+      password: "admin",
+    },
+    {
+      username: "user",
+      password: "user",
+    }
+  ]);
+
+  const presetEvents = await Events.bulkCreate([
+    {
+      title: "Event 1",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Est lorem ipsum dolor sit amet consectetur adipiscing elit pellentesque. Purus faucibus ornare suspendisse sed nisi lacus. Pulvinar pellentesque habitant morbi tristique. Congue eu consequat ac felis donec et odio pellentesque. Vitae et leo duis ut diam quam nulla porttitor. Est ullamcorper eget nulla facilisi etiam dignissim. Viverra nam libero justo laoreet sit amet cursus sit. Ullamcorper a lacus vestibulum sed arcu non odio. Odio euismod lacinia at quis risus sed vulputate.",
+      location: "Location 1",
+      date: new Date(),
+      price: 0,
+    },
+    {
+      title: "Event 2",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Est lorem ipsum dolor sit amet consectetur adipiscing elit pellentesque. Purus faucibus ornare suspendisse sed nisi lacus. Pulvinar pellentesque habitant morbi tristique. Congue eu consequat ac felis donec et odio pellentesque. Vitae et leo duis ut diam quam nulla porttitor. Est ullamcorper eget nulla facilisi etiam dignissim. Viverra nam libero justo laoreet sit amet cursus sit. Ullamcorper a lacus vestibulum sed arcu non odio. Odio euismod lacinia at quis risus sed vulputate.",
+      location: "Location 2",
+      date: new Date(),
+      price: 0,
+    },
+    {
+      title: "Event 3",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Est lorem ipsum dolor sit amet consectetur adipiscing elit pellentesque. Purus faucibus ornare suspendisse sed nisi lacus. Pulvinar pellentesque habitant morbi tristique. Congue eu consequat ac felis donec et odio pellentesque. Vitae et leo duis ut diam quam nulla porttitor. Est ullamcorper eget nulla facilisi etiam dignissim. Viverra nam libero justo laoreet sit amet cursus sit. Ullamcorper a lacus vestibulum sed arcu non odio. Odio euismod lacinia at quis risus sed vulputate.",
+      location: "Location 3",
+      date: new Date(),
+      price: 0,
+    },
+  ]);
+  presetUsers;
+  presetEvents;
+
   res.send("API is running");
 });
 
