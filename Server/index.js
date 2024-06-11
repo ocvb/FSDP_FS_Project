15c019f056adddc1c0755d723818ca0b026af55e
seuno <electrokai1@gmail.com>
2024-06-11 23:17:13 +0800

    hello

--- Server/index.js
+++ Server/index.js
@@ -30,7 +30,7 @@ app.get("/api", async (req, res) => {
 
   const presetEvents = await Events.bulkCreate([
     {
-      title: "Event test",
+      title: "Event 1",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Est lorem ipsum dolor sit amet consectetur adipiscing elit pellentesque. Purus faucibus ornare suspendisse sed nisi lacus. Pulvinar pellentesque habitant morbi tristique. Congue eu consequat ac felis donec et odio pellentesque. Vitae et leo duis ut diam quam nulla porttitor. Est ullamcorper eget nulla facilisi etiam dignissim. Viverra nam libero justo laoreet sit amet cursus sit. Ullamcorper a lacus vestibulum sed arcu non odio. Odio euismod lacinia at quis risus sed vulputate.",
       location: "Location 1",
       date: new Date(),
