471eee62254e4bc91c3c6680c8023db5254385a0
seuno <electrokai1@gmail.com>
2024-06-12 22:55:56 +0800

    Added new API for user updae

--- Server/api/Account.js
+++ Server/api/Account.js
@@ -2,17 +2,25 @@ const express = require("express");
 const router = express.Router();
 const { Users } = require("../model");
 
-router.post("/api/login", async (req, res) => {
-    const { username, password } = req.body;
-    const user = await Users.findOne({ where: { username, password } });
+// TODO: JWT Authentication, Password hashing
+
+router.post("/api/user", async (req, res) => {
+    const { username, password, uuid } = req.body;
+    var user;
+    if (uuid) {
+        user = await Users.findOne({ where: { uuid } });
+    } else {
+        user = await Users.findOne({ where: { username, password } });
+    }
+
     if (user) {
         res.json({ status: "success", data: user });
     } else {
-        res.sendStatus(401);
+        res.json({ status: "error", message: "Invalid username or password" })
     }
 });
 
-router.post("/api/register", async (req, res) => {
+router.post("/api/user/register", async (req, res) => {
     const { username, password } = req.body;
     const findSameUsername = await Users.findOne({ where: { username } });
     if (findSameUsername) {
@@ -25,6 +33,24 @@ router.post("/api/register", async (req, res) => {
         res.json({ status: "success", data: user });
     } else {
         res.sendStatus(500);
+    }
+});
+
+router.put("/api/user/update/:uuid", async (req, res) => {
+    const { username, password } = req.body;
+    const { uuid } = req.params;
+
+    try {
+        const user = await Users.update({ username, password }, { where: { uuid } }, { returning: true });
+
+        if (user == 1) { // update returns an array where the first element is the number of affected rows
+            res.json({ status: "success", data: user });
+        } else {
+            res.status(404).json({ status: "error", message: "User not found" });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ status: "error", message: "An error occurred while updating the user" });
     }
 });
 
