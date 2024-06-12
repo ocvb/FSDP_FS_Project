834a12d536166237d948c843eec791b9e42098a9
seuno <electrokai1@gmail.com>
2024-06-12 22:55:32 +0800

    User Profile now can Update their Information

--- Client/src/routes/Account/Profile/UserProfile.jsx
+++ Client/src/routes/Account/Profile/UserProfile.jsx
@@ -1,22 +1,46 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import { TextField } from "@mui/material";
-import PasswordVisibility from "../../../components/PasswordVIsibility/PasswordVisibility.module";
-import Button from "../../../components/Button/CustomButton.module";
+import PasswordVisibility from "@/components/PasswordVIsibility/PasswordVisibility.module";
+import Button from "@/components/Button/CustomButton.module";
 
 import mainStyles from "./css/Profile.module.css";
 import styles from "./css/UserProfile.module.css";
+import { useNavigate } from "react-router-dom";
 
 export default function UserProfile({ user }) {
+    const [retrieveUser, setRetrieveUser] = useState({});
 
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
-    useEffect(() => {
-        setUsername(user.username);
-        setPassword(user.password);
+    const [Message, setMessage] = useState('');
 
+    const navigate = useNavigate();
+
+    const checkUser = async (user) => {
+        await axios.post(`http://localhost:3001/api/user`, {
+            uuid: user,
+        }).then((response) => {
+            if (response.status != 200) {
+                console.error("User not found");
+                navigate("/account");
+                return;
+            }
+
+            setRetrieveUser(response.data['data']);
+        });
+    };
+
+    useEffect(() => {
+        checkUser(user);
     }, [user]);
+
+    useEffect(() => {
+        setUsername(retrieveUser.username || '');
+        setPassword(retrieveUser.password || '');
+    }, [retrieveUser]);
 
     const handleUsernameChange = (event) => {
         setUsername(event.target.value);
@@ -25,6 +49,40 @@ export default function UserProfile({ user }) {
     const handlePasswordChange = (event) => {
         setPassword(event.target.value);
     };
+
+    const updateUser = async () => {
+        let timeoutId = null;
+
+        // Update user information
+        await axios.put(`http://localhost:3001/api/user/update/${retrieveUser.uuid}`, {
+            username: username,
+            password: password,
+        }).then((response) => {
+            console.log(response.data)
+
+            if (response.status === 200) {
+                setRetrieveUser(response.data['data']);
+                setMessage("Updated");
+            }
+        }).catch((error) => {
+            console.error(error);
+        });
+
+
+        checkUser(user);
+
+        // Visual feedback
+        timeoutId = setTimeout(() => {
+            setMessage('Save changes');
+        }, 2000);
+        return () => clearTimeout(timeoutId);
+
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+
+    }
 
     return (
         <>
@@ -42,7 +100,7 @@ export default function UserProfile({ user }) {
                 padding: "1rem",
                 width: "100%",
             }}>
-                <form style={{
+                <form onSubmit={onSubmit} style={{
                     display: "flex",
                     flexDirection: "column",
                     gap: "1rem",
@@ -70,7 +128,7 @@ export default function UserProfile({ user }) {
                         }} />
                     </div>
 
-                    <Button text="Save changes" sx={{
+                    <Button text={Message ? Message : 'Save Changes'} type="submit" onClick={updateUser} sx={{
                         fontSize: "0.8rem",
                         display: "inline-flex",
                         borderRadius: "10px",
