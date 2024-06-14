62558e071bfadb7575d6e8d8e7fdf73c130f9c10
seuno <electrokai1@gmail.com>
2024-06-14 11:31:44 +0800

    Follow According to React structure

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0);
+// Components
+import NavigationBar from "@/components/Navbar/Navbar.module";
+import AuthProvider from "@/components/Auth/Auth";
+import ProtectedRoute from "@/components/Auth/RequireAuth";
 
+// Routes
+import Home from "./routes/Home/Home";
+import Events from "./routes/Events/Events";
+import Account from "./routes/Account/Account";
+
+import Profile from './routes/Account/Profile/Profile';
+
+import Admin from './routes/Account/Admin/Admin';
+
+import logo from "@/assets/Navbar/logo.png";
+
+import './index.css'
+
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+    <BrowserRouter>
+      <NavigationBar imgUrl={logo} />
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/account/*" element={<ProtectedRoute />}>
+            <Route path="admin" element={<Admin />} />
+            <Route path="profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter >
+  )
 }
-
-export default App;
--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,38 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-// Load Bootstrap CSS
-// import "bootstrap/dist/css/bootstrap.min.css";
-
-import "./index.css";
-
-// Components
-import NavigationBar from "./components/Navbar/Navbar.module";
-
-// Routes
-import Home from "./routes/Home/Home";
-import Events from "./routes/Events/Events";
-import Account from "./routes/Account/Account";
-
-import Profile from './routes/Account/Profile/Profile';
-import ProfileNotifications from './routes/Account/Profile/Notifications';
-// import { ProfileEvents } from './routes/Account/Profile/Events';
-
-// Logo
-import logo from "./assets/Navbar/logo.png";
-
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>
-      <NavigationBar imgUrl={logo} />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/events" element={<Events />} />
-        <Route path="/account/" element={<Account />} />
-        <Route path="/account/profile/" element={<Profile />} />
-      </Routes>
-    </Router>
+    <App />
   </React.StrictMode>
 );
