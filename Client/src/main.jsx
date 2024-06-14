59b51f229cecb0314dbf30c0ce5d091e87fb50ee
seuno <electrokai1@gmail.com>
2024-06-14 22:44:11 +0800

    Moved AuthProvider & BrowserRouter to Main.jsx

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
+import AuthProvider from "@/components/Auth/Auth";
 import App from "./App";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>
 );
