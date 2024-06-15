65ece8d622694da40807f6c5d098d048bed505fc
seuno <electrokai1@gmail.com>
2024-06-16 03:31:10 +0800

    obfuscate code, and performance improvements

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -2,15 +2,66 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from 'path';
 
+import obfuscator from 'rollup-plugin-obfuscator';
+import { VitePWA } from 'vite-plugin-pwa';
+
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    obfuscator({
+      global: true,
+    }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'People\'s Project',
+        short_name: 'App',
+        description: 'People\'s Project is a platform for the people',
+        theme_color: '#ffffff',
+        // icons: [
+        //   {
+        //     src: 'pwa-192x192.png',
+        //     sizes: '192x192',
+        //     type: 'image/png',
+        //   },
+        //   {
+        //     src: 'pwa-512x512.png',
+        //     sizes: '512x512',
+        //     type: 'image/png',
+        //   },
+        //   {
+        //     src: 'pwa-512x512.png',
+        //     sizes: '512x512',
+        //     type: 'image/png',
+        //     purpose: 'any maskable',
+        //   },
+        // ],
+      },
+    }),
+  ],
   server: {
-    port: 3000,
-    proxy: "http://localhost:3000",
+    hmr: {
+      protocol: 'ws',
+      host: 'localhost',
+      port: '3000'
+    }
   },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     }
-  }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
+    },
+  },
 });
