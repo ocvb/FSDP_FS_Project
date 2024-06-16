0f006aaaa64a4b576cc05e871d9caf81ef0ce7f2
seuno <electrokai1@gmail.com>
2024-06-16 11:26:10 +0800

    ew whitespaces

--- Client/src/components/Button/CustomButton.module.jsx
+++ Client/src/components/Button/CustomButton.module.jsx
@@ -1,8 +1,11 @@
 import { forwardRef } from "react";
+import PropTypes from "prop-types";
+
 
 import { Button } from "@mui/material";
 
-const CustomButton = forwardRef(({ className, onClick, type, sx, text, onMouseDown, onMouseLeave, onMouseEnter }, ref) => {
+
+const CustomButton = forwardRef(({ className, onClick, type, sx, text, onMouseDown, onMouseLeave, onMouseEnter, startIcon, endIcon }, ref) => {
     return (
         <Button ref={ref} sx={{
             // "&:hover": {
@@ -16,11 +19,26 @@ const CustomButton = forwardRef(({ className, onClick, type, sx, text, onMouseDo
                 outline: 'none',
             },
             ...sx,
-        }} className={className} onClick={onClick} type={type} onMouseDown={onMouseDown} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
+        }} className={className} onClick={onClick} type={type} onMouseDown={onMouseDown} onMouseEnter={onMouseEnter} startIcon={startIcon} endIcon={endIcon} onMouseLeave={onMouseLeave}>
             {text}
         </Button>
     );
 });
+
+CustomButton.displayName = 'CustomButton';
+
+CustomButton.propTypes = {
+    className: PropTypes.string,
+    onClick: PropTypes.func,
+    type: PropTypes.string,
+    sx: PropTypes.object,
+    text: PropTypes.string,
+    onMouseDown: PropTypes.func,
+    onMouseLeave: PropTypes.func,
+    onMouseEnter: PropTypes.func,
+    startIcon: PropTypes.element,
+};
+
 
 
 
