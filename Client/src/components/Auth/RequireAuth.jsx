93d5b9a09a999db9d2dd303537c9b71089c7b728
seuno <electrokai1@gmail.com>
2024-06-16 11:24:37 +0800

    use fetchAuth for latest update

--- Client/src/components/Auth/RequireAuth.jsx
+++ Client/src/components/Auth/RequireAuth.jsx
@@ -1,8 +1,9 @@
 import { Navigate, useLocation, Outlet } from 'react-router-dom';
 import { UseAuth } from '@/components/Auth/Auth';
+import { useEffect } from 'react';
 export default function ProtectedRoute() {
-    const { isAuthenticated } = UseAuth();
+    const { fetchAuth } = UseAuth();
     const location = useLocation();
 
-    return isAuthenticated ? <Outlet /> : <Navigate to="/account" replace state={{ path: location.pathname }} />;
-}   
+    return fetchAuth().isAuthenticated ? <Outlet /> : <Navigate to="/account" replace state={{ path: location.pathname }} />;
+}
