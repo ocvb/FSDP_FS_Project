f96e709c029a499d3a92e04f5b0d26e8883c25c5
seuno <electrokai1@gmail.com>
2024-06-16 11:27:24 +0800

    styling for anchor

--- Client/src/components/Dropdown/Dropdown.module.jsx
+++ Client/src/components/Dropdown/Dropdown.module.jsx
@@ -5,6 +5,13 @@ import styles from "./css/Dropdown.module.css";
 
 const Dropdown = memo(forwardRef(({ subitems = [], dropdown, onMouseLeave }, ref) => {
 
+    const anchorStyle = {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: "0.2rem",
+    }
+
     return (
         <div
             className={styles.dropdown}
@@ -16,7 +23,7 @@ const Dropdown = memo(forwardRef(({ subitems = [], dropdown, onMouseLeave }, ref
             }}>
             <div className={styles.dropdownContent}>
                 {subitems.map((item, index) => (
-                    <a key={index} className={styles.menuItem} href={item.href} onClick={item.action} >{item.name}  </a>
+                    <a key={index} className={styles.menuItem} href={item.href} onClick={item.action} style={anchorStyle}>{item.name} {item.icon} </a>
                 ))}
             </div>
         </div>
