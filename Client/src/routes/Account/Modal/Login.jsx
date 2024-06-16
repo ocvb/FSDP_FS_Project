f11f15d9822396c8465b57aea101aed80b4c0c98
seuno <electrokai1@gmail.com>
2024-06-16 11:29:27 +0800

    beauty changes, added LoginIcon

--- Client/src/routes/Account/Modal/Login.jsx
+++ Client/src/routes/Account/Modal/Login.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 import { UseAuth } from "@/components/Auth/Auth";
 import Button from "@/components/Button/CustomButton.module";
 import PasswordVisibility from "@/components/PasswordVIsibility/PasswordVisibility.module";
 import { TextField, colors } from "@mui/material";
+import LoginIcon from '@mui/icons-material/Login';
+import { CircularProgress } from "@mui/material";
 
 import styles from "./css/Modals.module.css";
 
@@ -25,10 +27,7 @@ export default function Login({ passToChangeModal }) {
     const [password, setPassword] = useState('');
     const [message, setMessage] = useState('');
     const [loginStatus, setloginStatus] = useState(false);
-
     const navigate = useNavigate();
-    const { state } = useLocation();
-
     const { login } = UseAuth();
 
     const checkLogin = () => {
@@ -38,22 +37,23 @@ export default function Login({ passToChangeModal }) {
             return;
         }
 
-        login({ username, password }).then((value) => {
-            if (value == true) {
-                setMessage("Logging in...");
-                setloginStatus(true);
-                setTimeout(() => {
-                    navigate(state?.path ? state.path : "/account/profile");
-                }, 1000);
-            } else {
+        login({ username, password })
+            .then((value) => {
+                if (value.result == true) {
+                    setloginStatus(true);
+                    setMessage("Logging in...");
+                    setTimeout(() => {
+                        return navigate(value.path, { replace: true });
+                    }, 1000);
+                } else {
+                    setloginStatus(false);
+                    setMessage("Invalid username or password");
+                }
+            }).catch((error) => {
+                console.log(error)
                 setloginStatus(false);
                 setMessage("Invalid username or password");
-            }
-        }).catch((error) => {
-            console.log(error)
-            setloginStatus(false);
-            setMessage("Invalid username or password");
-        });
+            });
     };
 
     const onSubmit = (event) => {
@@ -99,15 +99,20 @@ export default function Login({ passToChangeModal }) {
                 gap: "0.5rem",
                 width: "100%",
             }}>
-                <Button text={loginStatus ? message : "login"} type='submit' sx={{
-                    borderRadius: "10px",
-                    backgroundColor: "black",
-                    padding: "0.5rem 2rem",
-                    color: "white",
-                    '&:hover': {
-                        backgroundColor: "rgb(0, 0, 0, 0.85)",
-                    },
-                }} />
+                <Button
+                    text={loginStatus ? message : "login"}
+                    type='submit'
+                    startIcon={loginStatus ? <CircularProgress size={20} /> : <LoginIcon />}
+                    sx={{
+                        borderRadius: "10px",
+                        backgroundColor: "black",
+                        padding: "0.5rem 2rem",
+                        color: "white",
+                        '&:hover': {
+                            backgroundColor: "rgb(0, 0, 0, 0.85)",
+                        },
+
+                    }} />
                 <p style={{
                     color: colors.grey[700],
                     fontSize: "1rem",
