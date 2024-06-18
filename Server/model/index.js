803e59063152ba63136b0fab79f36ce37041aefb
seuno <electrokai1@gmail.com>
2024-06-18 20:30:09 +0800

    Foreign Keys for user & event

--- Server/model/index.js
+++ Server/model/index.js
@@ -52,46 +52,16 @@ const Events = db.define("events", {
   },
 });
 
-
-
-// class User extends Model {}
-
-// class Post extends Model {}
-
-// User.init(
-//   {
-//     username: DataTypes.STRING,
-//     birthday: DataTypes.DATE,
-//   },
-//   { sequelize, modelName: "user" }
-// );
-
-// Post.init(
-//   {
-//     title: DataTypes.STRING,
-//     body: DataTypes.TEXT,
-//   },
-//   { sequelize, modelName: "post" }
-// );
-
-// (async function () {
-//   try {
-//     await sequelize.sync();
-
-//     const jane = await User.create({
-//       username: "janedoe",
-//       birthday: new Date(1980, 6, 20),
-//     });
-//     console.log(jane.toJSON());
-
-//     const post = await Post.create({
-//       title: "First Post",
-//       body: "This is the first post in the blog",
-//     });
-//     console.log(post.toJSON());
-//   } catch (error) {
-//     console.error("Error:", error);
-//   }
-// })();
+Users.hasMany(Events), {
+  foreignKey: {
+    'name': 'eventId',
+  },
+};
+Events.belongsTo(Users, {
+  foreignKey: {
+    'name': 'userId',
+  },
+  constraints: false,
+});
 
 module.exports = { db, Users, Events };
