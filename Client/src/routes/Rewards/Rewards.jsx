b079680a0023fd6ca9806c7d93e4f1245a732938
eh-student <233539X@mymail.nyp.edu.sg>
2024-06-26 02:50:23 +0800

    Public Rewards page content and styling

--- Client/src/routes/Rewards/Rewards.jsx
+++ Client/src/routes/Rewards/Rewards.jsx
@@ -1,46 +1,69 @@
+import { colors } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
+import headerImage from "@/assets/Rewards/rewards-header.jpg";
+
+import style from "./css/Rewards.module.css";
+
+// Components
 import CustomButton from "@/components/Button/CustomButton.module";
 import Footer from "@/components/Footer/Footer.module";
 
-const Header = () => (
-  <header className="header">
-    <img
-      src="fsdp/Client/src/assets/Rewards/rewards-header.jpg"
-      className="header-image"
-    />
-    <div className="header-text">
-      <h1>Rewards</h1>
-      <p>Create an account to earn points and claim exciting rewards!</p>
-      <CustomButton className="sign-up-button" text="Sign Up" link="/account" />
-    </div>
-  </header>
-);
+const Header = () => {
+  const navigate = useNavigate();
+  return (
+    <header className={style.header}>
+      <img src={headerImage} className={style.headerImage} alt="Header" />
+      <div className={style.headerText}>
+        <h1>Rewards</h1>
+        <p>Create an account to earn points and claim exciting rewards!</p>
+        <CustomButton
+          text="Sign Up Now"
+          onClick={() => navigate("/account")}
+          sx={{
+            display: "inline-flex",
+            borderRadius: "50px",
+            width: "fit-content",
+            boxShadow: "0px 2px 6px 0px rgba(0, 0, 0, 0.15)",
+            padding: "0.4rem 2rem",
+            color: "black",
+            backgroundColor: "white",
+            "&:hover": {
+              backgroundColor: colors.grey[300],
+            },
+          }}
+        />
+      </div>
+    </header>
+  );
+};
 
 const Partners = () => (
-  <div className="partners">
+  <div className={style.partners}>
     <h2>Partnered with</h2>
-    <div className="partners-logos">
-      <img src="" alt="" />
-      <img src="" alt="" />
-      <img src="" alt="" />
-      <img src="" alt="" />
-      <img src="" alt="" />
+    <div className={style.partnersLogos}>
+      <img src="https://via.placeholder.com/150" alt="Partner 1" />
+      <img src="https://via.placeholder.com/150" alt="Partner 2" />
+      <img src="https://via.placeholder.com/150" alt="Partner 3" />
+      <img src="https://via.placeholder.com/150" alt="Partner 4" />
+      <img src="https://via.placeholder.com/150" alt="Partner 5" />
     </div>
   </div>
 );
 
 const Steps = () => (
-  <div className="how-it-works">
+  <div className={style.howItWorks}>
     <h2>How it works</h2>
-    <div className="steps">
-      <div className="step">
+    <div className={style.steps}>
+      <div className={style.step}>
         <h3>1</h3>
         <p>Earn points by participating/volunteering at events</p>
       </div>
-      <div className="step">
+      <div className={style.step}>
         <h3>2</h3>
         <p>Collect sufficient points and redeem rewards of your choice</p>
       </div>
-      <div className="step">
+      <div className={style.step}>
         <h3>3</h3>
         <p>Use your rewards!</p>
       </div>
@@ -51,12 +74,12 @@ const Steps = () => (
 export default function Rewards() {
   return (
     <>
-    <div>
-      <Header />
-      <Partners />
-      <Steps />
-    </div>
-    <Footer />
+      <div>
+        <Header />
+        <Partners />
+        <Steps />
+      </div>
+      <Footer />
     </>
   );
 }
