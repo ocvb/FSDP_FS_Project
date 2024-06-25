489a43234db8fa646dc90df0647b01f0691a3ffe
kevin <kevinzx76@gmail.com>
2024-06-25 15:46:15 +0800

    added image

--- Client/src/routes/Courses/Courses.jsx
+++ Client/src/routes/Courses/Courses.jsx
@@ -1,6 +1,6 @@
 import { Box, Container } from "@mui/material";
-
-import "./css/Courses.module.css";
+import images from "@/assets/Courses/Courses.jpg";
+import styles from "./css/Courses.module.css";
 
 export default function Courses() {
     return (
@@ -8,10 +8,15 @@ export default function Courses() {
             component={Container}
             maxWidth={true}>
             <Container>
-                <div>
-                    <h1>Courses</h1>
-                    <h2>Ever wanted to learn a new skill or pick up a new hobby?</h2>
-                    <h3>Then apply for one of our many courses to upgrade yourself!</h3>
+                <div styles="position: relative;">
+                    <div className={styles.header}>
+                        <img src={images} className={styles.img}></img>
+                        <div className={styles.header_details}>
+                            <h1>Courses</h1>
+                            <h2>Ever wanted to learn a new skill or pick up a new hobby?</h2>
+                            <h3>Then apply for one of our many courses to upgrade yourself!</h3>
+                        </div>
+                    </div>
                 </div>
             </Container>
         </Box>
