90835a90da272e0eb9a6a1e85297e3d5660ddb92
kevin <kevinzx76@gmail.com>
2024-06-25 16:18:19 +0800

    minor change

--- Client/src/routes/Courses/Courses.jsx
+++ Client/src/routes/Courses/Courses.jsx
@@ -1,24 +1,19 @@
-import { Box, Container } from "@mui/material";
+import { Container } from "@mui/material";
 import images from "@/assets/Courses/Courses.jpg";
 import styles from "./css/Courses.module.css";
 
 export default function Courses() {
+
     return (
-        <Box
-            component={Container}
-            maxWidth={true}>
-            <Container>
-                <div styles="position: relative;">
-                    <div className={styles.header}>
-                        <img src={images} className={styles.img}></img>
-                        <div className={styles.header_details}>
-                            <h1>Courses</h1>
-                            <p className={styles.p}>Ever wanted to learn a new skill or pick up a new hobby?</p>
-                            <p className={styles.p}>Then apply for one of our many courses to upgrade yourself!</p>
-                        </div>
-                    </div>
+        <div styles="position: relative;">
+            <div className={styles.header}>
+                <img src={images} className={styles.img}></img>
+                <div className={styles.header_details}>
+                    <h1>Courses</h1>
+                    <p className={styles.p}>Ever wanted to learn a new skill or pick up a new hobby?</p>
+                    <p className={styles.p}>Then apply for one of our many courses to upgrade yourself!</p>
                 </div>
-            </Container>
-        </Box>
+            </div>
+        </div>
     );
 }
