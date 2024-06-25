326a64f57930c55f4d00c7fc78ba65825401cd6c
seuno <electrokai1@gmail.com>
2024-06-25 11:10:39 +0800

    add QueryClientProvider from tanstack

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -3,14 +3,18 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import AuthProvider from "@/components/Auth/Auth";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
 
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
