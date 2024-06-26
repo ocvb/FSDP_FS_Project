586c858816aac32efbf83672b1a5ac50f9cb4fd2
kevin <kevinzx76@gmail.com>
2024-06-26 10:17:07 +0800

    api backend

--- Server/api/Courses.js
+++ Server/api/Courses.js
@@ -1,9 +1,27 @@
 const express = require("express");
 const router = express.Router();
 const { TokenAuthentication } = require("./Middlewares/TokenAuthentication");
+const { Courses } = require("../model");
 
 router.get('/user', TokenAuthentication, async (req, res) => {
     const { userId } = req.query;
+
+    if (userId) {
+        const coursesByUserId = await Events.findAll({
+            where: { userId: userId },
+            attributes: ['id', 'title', 'description']
+        });
+
+        console.log(coursesByUserId)
+        if (coursesByUserId.length > 0) {
+            res.status(200).json(coursesByUserId);
+        } else {
+            res.status(404).json({ message: 'No courses found for this user' });
+        }
+    } else {
+        const courses = await Courses.findAll();
+        res.status(200).json(courses);
+    }
 });
 
 router.get('/', async (req, res) => {
@@ -11,11 +29,13 @@ router.get('/', async (req, res) => {
         res.json(courses);
 });
 
-router.post('api/courses', async (req, res) => {
-    const course = new course({
-        title: req.body.title,
-        description: req.body.description,
-    });
+router.post('api/courses', TokenAuthentication, async (req, res) => {
+    const { title, description } = req.body;
+    if (title.length == 0 || description.length == 0){
+        res.status(400).json({ message: "Please enter the title and description." });
+        return;
+    }
+
     
     try {
         const newCourse = await course.save();
@@ -24,3 +44,5 @@ router.post('api/courses', async (req, res) => {
         res.status(400).json({ message: err.message });
     }
 });
+
+module.exports = router;
