8b471a48c4041d510a313b170b0092cbd38d9318
seuno <electrokai1@gmail.com>
2024-06-27 12:45:50 +0800

    Dropdown.module.jsx disable clicks while dropdown is active

--- Client/src/components/Dropdown/Dropdown.module.jsx
+++ Client/src/components/Dropdown/Dropdown.module.jsx
@@ -3,31 +3,47 @@ import PropTypes from "prop-types";
 
 import styles from "./css/Dropdown.module.css";
 
-const Dropdown = memo(forwardRef(({ subitems = [], dropdown, onMouseLeave }, ref) => {
+const Dropdown = memo(forwardRef(({ subitems = [], dropdown, onMouseLeave, onMouseDown, style, allowHover }, ref) => {
 
     const anchorStyle = {
         display: "flex",
         alignItems: "center",
         justifyContent: "start",
-        gap: "0.2rem",
+        gap: "0.5rem",
     }
 
-
     return (
-        <div
-            className={styles.dropdown}
-            ref={ref}
-            onMouseLeave={onMouseLeave}
-            style={{
-                display: dropdown ? "block" : "none",
-                position: "absolute",
-            }}>
-            <div className={styles.dropdownContent}>
-                {subitems.map((item, index) => (
-                    <a key={index} className={styles.menuItem} href={item.href} onMouseDown={item.action} style={anchorStyle}>{item.icon} {item.name}</a>
-                ))}
+        <>
+            {!allowHover && dropdown && <div style={{
+                position: "fixed",
+                top: 0,
+                left: 0,
+                width: "100vw",
+                height: "100vh",
+                pointerEvents: "auto",
+                zIndex: 2,
+                // backgroundColor: "rgba(0, 0, 0, 0.1)",
+            }} onClick={onMouseLeave}></div>}
+
+            <div
+                className={styles.dropdown}
+                ref={ref}
+                onMouseDown={onMouseDown}
+                onMouseLeave={onMouseLeave}
+                style={{
+                    display: dropdown ? "block" : "none",
+                    position: "absolute",
+                    zIndex: 3,
+                    fontSize: "1rem",
+                    ...style,
+                }}>
+                <div className={styles.dropdownContent}>
+                    {subitems.map((item, index) => (
+                        <a key={index} className={styles.menuItem} href={item.href} onMouseDown={item.action} style={anchorStyle}>{item.icon} {item.name}</a>
+                    ))}
+                </div>
             </div>
-        </div>
+        </>
     );
 }));
 
