9514a26994ee4f3a93b70662d2b064c3b2de722d
seuno <electrokai1@gmail.com>
2024-06-28 21:05:10 +0800

    Files: Rename to TSX

---
+++ Client/src/App.tsx
@@ -0,0 +1,43 @@
+import { Route, Routes, useLocation } from "react-router-dom";
+
+// Components
+import NavigationBar from "@/components/Navbar/Navbar.module";
+import NavbarAdmin from "./components/Navbar/NavbarAdmin.module";
+import { UseAuth } from "@/components/Auth/Auth";
+import ProtectedRoute from "@/components/Auth/RequireAuth";
+
+// Routes
+import Home from "./routes/Home/Home";
+import Events from "./routes/Events/Events";
+import Account from "./routes/Account/Account";
+import SkillShare from "./routes/SkillShare/SkillShare";
+
+import Profile from './routes/Account/Profile/Profile';
+
+import Admin from './routes/Account/Admin/Admin';
+
+import logo from "@assets/Navbar/logo.png" ;
+
+import './index.css'
+
+export default function App() {
+  const { fetchAuth } = UseAuth();
+  const location = useLocation();
+  const checkIfAdmin = fetchAuth().userRole === "admin";
+  const isAdminRoute = location.pathname.includes("admin");
+  return (
+    <>
+      {checkIfAdmin && isAdminRoute ? <NavbarAdmin logo={logo} /> : <NavigationBar imgUrl={logo} />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/skill-share" element={<SkillShare />} />
+        <Route path="/account" element={<Account />} />
+        <Route path="/account/*" element={<ProtectedRoute />}>
+          <Route path="admin" element={<Admin />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
+      </Routes>
+    </>
+  )
+}
---
+++ Client/src/main.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+import AuthProvider from "@/components/Auth/Auth";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+import App from "./App";
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <QueryClientProvider client={queryClient}>
+          <App />
+          <ReactQueryDevtools />
+        </QueryClientProvider>
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
