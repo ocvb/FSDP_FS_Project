3cd8d43b96fc8f9b9797082f99f91551afc4ed2e
Kevin <234313n@mymail.nyp.edu.sg>
2024-07-17 10:16:29 +0800

    minor

--- Client/src/routes/Courses/Courses.tsx
+++ Client/src/routes/Courses/Courses.tsx
@@ -44,7 +44,7 @@ export default function Courses() {
     useEffect(() => {
         async function fetchCourses() {
             const response = await axios.get(
-                'http://localhost:3001/api/courses'
+                'http://localhost:3001/api/admin/courses'
             );
             setCourses(response.data);
         }
