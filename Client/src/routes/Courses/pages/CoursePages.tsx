2f2941d3c0dcc44667e7b2cebcbe14e81c553429
Kevin <234313n@mymail.nyp.edu.sg>
2024-07-25 11:16:24 +0800

    course pages routing

--- Client/src/routes/Courses/pages/CoursePages.tsx
+++ Client/src/routes/Courses/pages/CoursePages.tsx
@@ -1,3 +1,11 @@
 import HealthWellness from './HealthWellness';
+import LifeLong from './LifelongLeisure';
+import SportsFitness from './SportsFitness'
+import LifeLongLearning from './LifelongLearning';
+import EducationEnrichment from './EducationEnrichment';
 
 export { HealthWellness };
+export { LifeLong };
+export { SportsFitness };
+export { LifeLongLearning };
+export { EducationEnrichment };
--- Client/src/routes/Courses/pages/LifelongLearning.tsx
+++ Client/src/routes/Courses/pages/LifelongLearning.tsx
@@ -2,13 +2,13 @@ import { Container, Button } from '@mui/material';
 import images from '@/assets/Courses/Courses.jpg';
 import styles from '@/routes/Courses/css/Courses.module.css';
 
-export default function HealthWellness() {
+export default function LifeLongLearning() {
     return (
         <div style={{ position: 'relative' }}>
             <div className={styles.header}>
                 <img src={images} className={styles.img} alt='Courses' />
                 <div className={styles.header_details}>
-                    <h1>Health & Wellness</h1>
+                    <h1>Lifelong Learning</h1>
                     <br />
                     <p className={styles.p}>
                         These are the courses we have available for this category!
