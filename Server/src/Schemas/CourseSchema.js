44ce6d4298e20a0c2ae01e8747e0102d110c7711
Kevin <234313n@mymail.nyp.edu.sg>
2024-07-27 18:01:04 +0800

    schema

--- Server/src/Schemas/CourseSchema.js
+++ Server/src/Schemas/CourseSchema.js
@@ -3,4 +3,5 @@ const yup = require('yup');
 exports.CourseSchema = yup.object().shape({
     title: yup.string().min(1).required(),
     description: yup.string().min(1).required(),
+    category: yup.string().min(1).required()
 });
