9ed3e17a791c29569b460beddaff1e4ae7fbb5fe
seuno <electrokai1@gmail.com>
2024-07-28 23:16:21 +0800

    Client[App.tsx]: Dark Mode support yet to complete.

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -10,7 +10,13 @@ import { UseAuth } from '@components/Auth/Auth';
 import Home from '@routes/Home/Home';
 import Events from '@routes/Events/Events';
 import Courses from '@routes/Courses/Courses';
-import { EducationEnrichment, HealthWellness, LifeLongLearning, LifestyleLeisure, SportsFitness } from '@routes/Courses/pages/CoursePages';
+import {
+    EducationEnrichment,
+    HealthWellness,
+    LifeLongLearning,
+    LifestyleLeisure,
+    SportsFitness,
+} from '@routes/Courses/pages/CoursePages';
 import Account from '@routes/Account/Account';
 import SkillShare from '@routes/SkillShare/SkillShare';
 
@@ -21,38 +27,78 @@ import Admin from '@routes/Account/Admin/Admin';
 import logo from '@assets/Navbar/logo.png';
 
 import './index.css';
-import { headerFilteringStateInitializer } from '@mui/x-data-grid/internals';
+
+import {
+    createTheme,
+    CssBaseline,
+    PaletteMode,
+    ThemeProvider,
+} from '@mui/material';
+import { useEffect, useState } from 'react';
 
 export default function App() {
     const { fetchAuth } = UseAuth();
     const location = useLocation();
     const checkIfAdmin = fetchAuth.userRole === 'Admin';
     const isAdminRoute = location.pathname.includes('admin');
+
+    const [mode, setMode] = useState<PaletteMode>('light');
+
+    const theme = createTheme({
+        palette: {
+            mode: mode,
+            background: {
+                default: mode === 'light' ? '#ffffff' : '#000000',
+            },
+            text: {
+                primary: mode === 'light' ? '#000000' : '#ffffff',
+            },
+        },
+    });
+
     return (
         <>
-            {checkIfAdmin && isAdminRoute ? (
-                <NavbarAdmin logo={logo} />
-            ) : (
-                <NavigationBar imgUrl={logo} />
-            )}
-            <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/events' element={<Events />} />
-                <Route path='/courses/*' element={<Courses />} />
-                <Route path='/courses/*' element={<Outlet />}>
-                    <Route path='HealthWellness' element={<HealthWellness />} />
-                    <Route path='LifestyleLeisure' element={<LifestyleLeisure />} />
-                    <Route path='SportsFitness' element={<SportsFitness />} />
-                    <Route path='EducationEnrichment' element={<EducationEnrichment />} />
-                    <Route path='LifelongLearning' element={<LifeLongLearning />} />
-                </Route>
-                <Route path='/skill-share' element={<SkillShare />} />
-                <Route path='/account' element={<Account />} />
-                <Route path='/account/*' element={<ProtectedRoute />}>
-                    <Route path='admin' element={<Admin />} />
-                    <Route path='profile' element={<Profile />} />
-                </Route>
-            </Routes>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                {checkIfAdmin && isAdminRoute ? (
+                    <NavbarAdmin logo={logo} />
+                ) : (
+                    <NavigationBar imgUrl={logo} />
+                )}
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/events' element={<Events />} />
+                    <Route path='/courses/*' element={<Courses />} />
+                    <Route path='/courses/*' element={<Outlet />}>
+                        <Route
+                            path='HealthWellness'
+                            element={<HealthWellness />}
+                        />
+                        <Route
+                            path='LifestyleLeisure'
+                            element={<LifestyleLeisure />}
+                        />
+                        <Route
+                            path='SportsFitness'
+                            element={<SportsFitness />}
+                        />
+                        <Route
+                            path='EducationEnrichment'
+                            element={<EducationEnrichment />}
+                        />
+                        <Route
+                            path='LifelongLearning'
+                            element={<LifeLongLearning />}
+                        />
+                    </Route>
+                    <Route path='/skill-share' element={<SkillShare />} />
+                    <Route path='/account' element={<Account />} />
+                    <Route path='/account/*' element={<ProtectedRoute />}>
+                        <Route path='admin' element={<Admin />} />
+                        <Route path='profile' element={<Profile />} />
+                    </Route>
+                </Routes>
+            </ThemeProvider>
         </>
     );
 }
