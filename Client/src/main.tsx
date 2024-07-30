8a3d6547860ec9f302b867d99a38dc9ec44546e4
seuno <electrokai1@gmail.com>
2024-07-31 00:43:44 +0800

    Update import path

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-import AuthProvider from '@components/Auth/Auth';
+import AuthProvider from '@contexts/Auth';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
@@ -20,5 +20,5 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
                 </QueryClientProvider>
             </AuthProvider>
         </BrowserRouter>
-    </React.StrictMode>,
+    </React.StrictMode>
 );
