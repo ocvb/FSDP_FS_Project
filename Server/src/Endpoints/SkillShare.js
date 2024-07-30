2e140a9d65bfc3a6c06306b14e1eb6ad82118121
seuno <electrokai1@gmail.com>
2024-07-31 02:06:30 +0800

    Client[Skillshare]: Fix endpoint, numberOfResponded is now null & implemented validation

--- Server/src/Endpoints/SkillShare.js
+++ Server/src/Endpoints/SkillShare.js
@@ -1,24 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const { SkillShares } = require('@models/index');
+const { SkillshareSchema } = require('@schema/SkillshareSchema');
 
+// GET route to fetch all skill shares
 router.get('/', async (req, res) => {
     const skillShares = await SkillShares.findAll();
     res.status(200).json(skillShares);
 });
 
-router.post('/:id', async (req, res) => {
-    const { id } = req.params;
-    const { title, description, postedBy, category, numberOfResponded } =
-        req.body;
-    const skillShare = await SkillShares.create({
-        title,
-        description,
-        postedBy,
-        category,
-        numberOfResponded,
-    });
-    res.status(201).json(skillShare);
+// POST route to create a new skill share
+router.post('/', async (req, res) => {
+    SkillshareSchema.validate(req.body)
+        .then(async (data) => {
+            const skillShare = await SkillShares.create({
+                ...data,
+            });
+            res.status(201).json(skillShare);
+        })
+        .catch((err) => {
+            return res.status(400).json({ message: err.errors[0] });
+        });
 });
 
 module.exports = router;
