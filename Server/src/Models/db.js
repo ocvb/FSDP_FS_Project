776f1927ce138f1f0b740d7d084a4baccf649e0b
seuno <electrokai1@gmail.com>
2024-07-31 02:30:39 +0800

    Server[db.js]: Update to use hosted database

--- Server/src/Models/db.js
+++ Server/src/Models/db.js
@@ -1,23 +1,22 @@
 const { Sequelize } = require('sequelize');
 const process = require('process');
 
-const db = new Sequelize({
-    dialect: 'sqlite',
-    storage: __dirname + '/' + process.env.db_file,
-    logging: false,
-});
-
-
-// // MYSQL
 // const db = new Sequelize({
-//     dialect: 'mysql',
-//     database: process.env.MYSQL_DB,
-//     username: process.env.MYSQL_USER,
-//     password: process.env.MYSQL_PASSWORD,
-//     host: process.env.MYSQL_HOST,
-//     port: process.env.MYSQL_PORT,
+//     dialect: 'sqlite',
+//     storage: __dirname + '/' + process.env.db_file,
 //     logging: false,
 // });
+
+// // MYSQL
+const db = new Sequelize({
+    dialect: 'mysql',
+    database: process.env.MYSQL_DB,
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    logging: false,
+});
 
 // let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
 
