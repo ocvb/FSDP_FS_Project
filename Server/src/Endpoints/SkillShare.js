dc63b3f91e0e4ca4bb09c81661e7ce58c427f41f
seuno <electrokai1@gmail.com>
2024-08-01 01:48:28 +0800

    Server[Skillshare]: Added validation middleware, change category min to 1

--- Server/src/Endpoints/SkillShare.js
+++ Server/src/Endpoints/SkillShare.js
@@ -2,25 +2,20 @@ const express = require('express');
 const router = express.Router();
 const { SkillShares } = require('@models/index');
 const { SkillshareSchema } = require('@schema/SkillshareSchema');
+const { SkillshareValidation } = require('@validations/SkillshareValidation');
 
 // GET route to fetch all skill shares
-router.get('/', async (req, res) => {
+router.get('/', async (_, res) => {
     const skillShares = await SkillShares.findAll();
     res.status(200).json(skillShares);
 });
 
 // POST route to create a new skill share
-router.post('/', async (req, res) => {
-    SkillshareSchema.validate(req.body)
-        .then(async (data) => {
-            const skillShare = await SkillShares.create({
-                ...data,
-            });
-            res.status(201).json(skillShare);
-        })
-        .catch((err) => {
-            return res.status(400).json({ message: err.errors[0] });
-        });
+router.post('/', SkillshareValidation, async (req, res) => {
+    const skillShare = await SkillShares.create({
+        ...req.validation,
+    });
+    res.status(201).json(skillShare);
 });
 
 module.exports = router;
