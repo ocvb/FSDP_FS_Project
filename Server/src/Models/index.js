5ba304b235f34bbb4efc438c7daabb552f3b38c9
seuno <electrokai1@gmail.com>
2024-08-01 11:37:49 +0800

    Server[Index.js]: Update relationships

--- Server/src/Models/index.js
+++ Server/src/Models/index.js
@@ -2,15 +2,20 @@ const db = require('@models/db');
 
 const Users = require('@models/Users');
 const Events = require('@models/Events');
+const UserEvents = require('@models/UserEvents');
 const Facilities = require('@models/Facilities');
 const SkillShares = require('@models/SkillShares');
 const Courses = require('@models/Courses');
 const Support = require('@models/Support');
 const Rewards = require('@models/Rewards');
 
+Users.belongsToMany(Events, { through: UserEvents, foreignKey: 'userId' });
+Events.belongsToMany(Users, { through: UserEvents, foreignKey: 'eventId' });
+
 module.exports = {
     Users,
     Events,
+    UserEvents,
     SkillShares,
     Facilities,
     Courses,
