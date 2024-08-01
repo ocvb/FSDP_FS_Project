a74b5ee5e7cf51ba2a21bcc0c79ec5f666f6c055
seuno <electrokai1@gmail.com>
2024-08-02 03:10:30 +0800

    Server[UserEvents.js]: Remove Created & Updated by

--- Server/src/Models/UserEvents.js
+++ Server/src/Models/UserEvents.js
@@ -3,23 +3,29 @@ const db = require('@models/db');
 const Users = require('@models/Users');
 const Events = require('@models/Events');
 
-const UserEvents = db.define('user_events', {
-    userId: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: Users,
-            key: 'id',
+const UserEvents = db.define(
+    'user_events',
+    {
+        userId: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: Users,
+                key: 'id',
+            },
+            allowNull: false,
         },
-        allowNull: false,
-    },
-    eventId: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: Events,
-            key: 'id',
+        eventId: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: Events,
+                key: 'id',
+            },
+            allowNull: false,
         },
-        allowNull: false,
     },
-});
+    {
+        timestamps: false,
+    }
+);
 
 module.exports = UserEvents;
