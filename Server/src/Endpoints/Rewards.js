99ede435697e8d3d9a241bfb76e510950cc2e317
seuno <electrokai1@gmail.com>
2024-08-02 11:39:30 +0800

    Server[Admin-Rewards.js]: Remove Update & Delete

--- Server/src/Endpoints/Rewards.js
+++ Server/src/Endpoints/Rewards.js
@@ -41,61 +41,61 @@ router.post("/", async (req, res) => {
 });
 
 // Update a reward
-router.put("/:id", TokenAuthentication, async (req, res) => {
-  const { id } = req.params;
-  const { title, description, points, claimed } = req.body;
+// router.put("/:id", TokenAuthentication, async (req, res) => {
+//   const { id } = req.params;
+//   const { title, description, points, claimed } = req.body;
 
-  if (!title || !description || !points) {
-    res.status(400).json({ message: "Please provide all the required fields" });
-    return;
-  }
-  console.log(req.body)
-  try {
+//   if (!title || !description || !points) {
+//     res.status(400).json({ message: "Please provide all the required fields" });
+//     return;
+//   }
+//   console.log(req.body)
+//   try {
 
-    const reward = await Rewards.upsert({
-      id,
-      title: title,
-      description: description,
-      points: points,
-      claimed: claimed || false,
-    }, {
-      where: {
-        id: id
-      }
-    })
-    res.status(200).json({ message: "Reward updated successfully", reward });
-    // const reward = await Rewards.findByPk(id);
-    // if (reward) {
-    //   reward.title = title;
-    //   reward.description = description;
-    //   reward.points = points;
-    //   reward.claimed = claimed !== undefined ? claimed : reward.claimed;
-    //   await reward.save();
-    //   console.log(reward)
-    // } else {
-    //   res.status(404).json({ message: "Reward not found" });
-    // }
-  } catch (error) {
-    console.error("Error updating reward:", error);
-    res.status(500).json({ message: "Error updating reward", error });
-  }
-});
+//     const reward = await Rewards.upsert({
+//       id,
+//       title: title,
+//       description: description,
+//       points: points,
+//       claimed: claimed || false,
+//     }, {
+//       where: {
+//         id: id
+//       }
+//     })
+//     res.status(200).json({ message: "Reward updated successfully", reward });
+//     // const reward = await Rewards.findByPk(id);
+//     // if (reward) {
+//     //   reward.title = title;
+//     //   reward.description = description;
+//     //   reward.points = points;
+//     //   reward.claimed = claimed !== undefined ? claimed : reward.claimed;
+//     //   await reward.save();
+//     //   console.log(reward)
+//     // } else {
+//     //   res.status(404).json({ message: "Reward not found" });
+//     // }
+//   } catch (error) {
+//     console.error("Error updating reward:", error);
+//     res.status(500).json({ message: "Error updating reward", error });
+//   }
+// });
 
-// Delete a reward
-router.delete("/:id", TokenAuthentication, async (req, res) => {
-  const { id } = req.params;
-  try {
-    const reward = await Rewards.findByPk(id);
-    if (reward) {
-      await reward.destroy();
-      res.status(200).json({ message: "Reward deleted successfully" });
-    } else {
-      res.status(404).json({ message: "Reward not found" });
-    }
-  } catch (error) {
-    console.error("Error deleting reward:", error);
-    res.status(500).json({ message: "Error deleting reward", error });
-  }
-});
+// // Delete a reward
+// router.delete("/:id", TokenAuthentication, async (req, res) => {
+//   const { id } = req.params;
+//   try {
+//     const reward = await Rewards.findByPk(id);
+//     if (reward) {
+//       await reward.destroy();
+//       res.status(200).json({ message: "Reward deleted successfully" });
+//     } else {
+//       res.status(404).json({ message: "Reward not found" });
+//     }
+//   } catch (error) {
+//     console.error("Error deleting reward:", error);
+//     res.status(500).json({ message: "Error deleting reward", error });
+//   }
+// });
 
 module.exports = router;
