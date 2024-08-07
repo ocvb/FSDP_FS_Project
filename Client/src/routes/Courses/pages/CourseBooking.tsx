88513472b76f6a8be3d3dde23788ba7a09703fdc
Kevin <234313n@mymail.nyp.edu.sg>
2024-08-07 11:25:15 +0800

    minor change

--- Client/src/routes/Courses/pages/CourseBooking.tsx
+++ Client/src/routes/Courses/pages/CourseBooking.tsx
@@ -1,14 +1,20 @@
 import React, { FormEvent, useState } from 'react';
-import { Container, TextField, Button, MenuItem, Select, InputLabel, FormControl } from '@mui/material';
+import {
+    Container,
+    TextField,
+    Button,
+    MenuItem,
+    Select,
+    InputLabel,
+    FormControl,
+} from '@mui/material';
 import { useLocation } from 'react-router-dom';
 import images from '@/assets/Courses/Courses.jpg';
 import styles from '@/routes/Courses/css/Courses.module.css';
 
-
 const CourseBooking: React.FC = () => {
     const location = useLocation<{ courses: Course[] }>();
     const courses = location.state?.courses || [];
-
 
     const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
@@ -29,7 +35,7 @@ const CourseBooking: React.FC = () => {
                 </div>
             </div>
             <Container
-                maxWidth="sm"
+                maxWidth='sm'
                 sx={{
                     display: 'flex',
                     flexDirection: 'column',
@@ -40,57 +46,61 @@ const CourseBooking: React.FC = () => {
             >
                 <form onSubmit={handleSubmit} className={styles.bookingForm}>
                     <TextField
-                        label="Name"
-                        variant="outlined"
+                        label='Name'
+                        variant='outlined'
                         fullWidth
-                        margin="normal"
+                        margin='normal'
                         required
                     />
                     <TextField
-                        label="Email"
-                        type="email"
-                        variant="outlined"
+                        label='Email'
+                        type='email'
+                        variant='outlined'
                         fullWidth
-                        margin="normal"
+                        margin='normal'
                         required
                     />
                     <TextField
-                        label="Course Title"
-                        type="title"
-                        variant="outlined"
+                        label='Course Title'
+                        type='title'
+                        variant='outlined'
                         fullWidth
-                        margin="normal"
+                        margin='normal'
                         required
                     />
-                    <FormControl fullWidth margin="normal" required>
-                        <InputLabel id="course-select-label">Course Category</InputLabel>
+                    <FormControl fullWidth margin='normal' required>
+                        <InputLabel id='course-select-label'>
+                            Course Category
+                        </InputLabel>
                         <Select
-                            labelId="course-select-label"
-                            label="Course Category"
+                            labelId='course-select-label'
+                            label='Course Category'
                             defaultValue={courses}
                         >
                             <MenuItem value={1}>Health & Wellness</MenuItem>
                             <MenuItem value={2}>Lifestyle & Leisure</MenuItem>
                             <MenuItem value={3}>Sports & Fitness</MenuItem>
-                            <MenuItem value={4}>Education & Enrichment</MenuItem>
+                            <MenuItem value={4}>
+                                Education & Enrichment
+                            </MenuItem>
                             <MenuItem value={5}>Lifelong Learning</MenuItem>
                         </Select>
                     </FormControl>
                     <TextField
-                        label="Preferred Start Date"
-                        type="date"
-                        variant="outlined"
+                        label='Preferred Start Date'
+                        type='date'
+                        variant='outlined'
                         fullWidth
-                        margin="normal"
+                        margin='normal'
                         InputLabelProps={{
                             shrink: true,
                         }}
                         required
                     />
                     <Button
-                        type="submit"
-                        variant="contained"
-                        color="primary"
+                        type='submit'
+                        variant='contained'
+                        color='primary'
                         className={styles.submitButton}
                     >
                         Book Course
