01bbd719ed793c0eae5852b30d8b77015c2a8356
seuno <electrokai1@gmail.com>
2024-08-10 00:20:40 +0800

    Server[index.js]: Error codes

--- Server/index.js
+++ Server/index.js
@@ -33,6 +33,21 @@ db.sync()
         console.log('Database is ready');
     })
     .catch((error) => {
+        if (error.code == 'ER_BAD_DB_ERROR') {
+            console.error('Database does not exist');
+        }
+        if (error.code == 'ER_ACCESS_DENIED_ERROR') {
+            console.error('Database username or password is incorrect');
+        }
+        if (error.code == 'ECONNREFUSED') {
+            console.error('Database connection refused');
+        }
+        if (error.code == 'ENOTFOUND') {
+            console.error(`Unable to connect ${error.syscall}`);
+        }
+        if (error.code == 'ETIMEDOUT') {
+            console.error('Connection Timed Out');
+        }
         console.error('Error:', error);
     });
 
