847820525124741fabf805eb51284aa8e9194f9a
seuno <electrokai1@gmail.com>
2024-08-10 20:34:57 +0800

    UserSkillshareREsponse.js: Added id to have increments

--- Server/src/Models/UserSkillshareResponse.js
+++ Server/src/Models/UserSkillshareResponse.js
@@ -1,19 +1,30 @@
 const { DataTypes } = require('sequelize');
 const db = require('@models/db');
 
-const UserSkillshareResponse = db.define('user_skillshare_response', {
-    userId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
+const UserSkillshareResponse = db.define(
+    'user_skillshare_response',
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        userId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        skillshareId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        response: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
     },
-    skillshareId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-    },
-    response: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-});
+    {
+        timestamps: true,
+    }
+);
 
 module.exports = UserSkillshareResponse;
